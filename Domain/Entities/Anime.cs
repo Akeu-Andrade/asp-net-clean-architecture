f0cf294657512602e5daeef481827598fdc9b882
Akeu-Andrade <akeusantana@gmail.com>
2024-04-06 21:03:22 -0300

    feat: adiciona entity Anime

---
+++ Domain/Entities/Anime.cs
@@ -0,0 +1,29 @@
+﻿using AnimesProtech.Domain.Specification;
+
+namespace AnimesProtech.Domain.Entities
+{
+    public class Anime : BaseEntity
+    {
+        private readonly AnimeNameSpecification _animeNameSpecification;
+
+        public string name { get; private set; }
+        public string? summary { get; set; }
+        public string? director { get; set; }
+
+        public Anime()
+        {
+            _animeNameSpecification = new AnimeNameSpecification();
+        }
+
+        public void ChangeName(string newName)
+        {
+            if (!_animeNameSpecification.IsSatisfiedBy(newName))
+            {
+                throw new ArgumentException("O nome não pode ser vazio ou nulo.", nameof(newName));
+            }
+
+            name = newName;
+        }
+
+    }
+}
---
+++ Domain/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace AnimesProtech.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public Guid id { get; set; }
+        public DateTimeOffset created_at { get; set; }
+        public DateTimeOffset? updated_at { get; set; }
+        public DateTimeOffset? deleted_at { get; set; }
+    }
+}
